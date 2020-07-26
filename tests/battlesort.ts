24de86ed39356bca9fa2f3e70a2a14b443309e11
Tim <moscro@gmail.com>
2020-07-25 21:15:18 -0700

    Hello Tests

---
+++ tests/battlesort.ts
@@ -0,0 +1,16 @@
+import Assert from 'power-assert'
+import {suite, suiteSetup, setup, test} from 'mocha'
+
+suite('Tree tests', function() {
+    suiteSetup(function() {
+        // No setup for the moment
+    })
+
+    setup(function() {
+        // No setup for the moment
+    })
+
+    test('A first, but wonderful, test', function() {
+        Assert.ok(true)
+    })
+})
